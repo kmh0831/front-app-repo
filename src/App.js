408a4f34773f29da979809aa42f08463ffceb441
kmh0831 <kmhyuk1018@gmail.com>
2024-08-23 09:18:06 +0000

    v1

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App(props) {
             align="center"
             className={classes.title}
           >
-            EKS DEMO Blog version-3
+            EKS DEMO Blog version-4
           </Typography>
           {new Date().toLocaleTimeString()}
         </Toolbar>
